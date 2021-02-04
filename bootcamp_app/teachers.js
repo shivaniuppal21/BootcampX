7a5cea67ddb56b8ab5cf452b182fba17d055b1e4
shivani <shivaniuppal21@gmail.com>
2021-02-04 02:05:30 +0000

    node postgres

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,16 +9,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name like '%${args[0]}%'
-LIMIT ${args[1] || 5};
+SELECT
+  DISTINCT teachers.name as teacher,
+  cohorts.name as cohort
+FROM
+  teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+WHERE
+  cohorts.name = '${args[0]}'
+ORDER BY
+  teacher;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+
+      console.log(`${args[0]}:${user.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
